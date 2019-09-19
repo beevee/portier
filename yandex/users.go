86337a32e0b048ac03098649c270a02c36804d59
Alexey Kirpichnikov <alex.kirp@gmail.com>
2019-09-19 13:37:39 +0500

    Add new API fields to users

--- yandex/users.go
+++ yandex/users.go
@@ -18,10 +18,12 @@ type User struct {
 	CostCenter string `json:"cost_center"`
 	IsActive   bool   `json:"is_active"`
 	Role       struct {
-		ID      string   `json:"role_id,omitempty"`
-		Name    string   `json:"name,omitempty"`
-		Limit   float32  `json:"limit,omitempty"`
-		Classes []string `json:"classes,omitempty"`
+		ID              string                   `json:"role_id,omitempty"`
+		Name            string                   `json:"name,omitempty"`
+		Limit           float32                  `json:"limit,omitempty"`
+		Classes         []string                 `json:"classes,omitempty"`
+		NoSpecificLimit bool                     `json:"no_specific_limit,omitempty"`
+		Restrictions    []map[string]interface{} `json:"restrictions,omitempty"`
 	} `json:"role"`
 }
 
